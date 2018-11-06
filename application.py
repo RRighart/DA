39665af79652276cf3c90cdc68dc3d442a283507
Ruthger Righart <rrighart@googlemail.com>
2018-11-06 14:00:38 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,47 +1,32 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
+# Beanstalk looks for application by default, if this isn't set you will get a WSGI error.
 application = app.server
 
-df = pd.DataFrame({'X':[1,2,3,4,5], 'Y':[1,3,9,16,27], 'Name':['A', 'B', 'C', 'D', 'E']})
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-df.to_csv('example_df.csv', index=False, header=True)
-df = pd.read_csv('example_df.csv')
+    html.Div(children='''
+        This is Dash running on Elastic Beanstalk.
+    '''),
 
-print(df.head())
-
-app.layout = html.Div([
     dcc.Graph(
-        id='Test X-Y',
+        id='example-graph',
         figure={
             'data': [
-                go.Scatter(
-                    x=df['X'],
-                    y=df['Y'],
-                    text=df['Name'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}}
-                    )        
+                {'x': [1, 2, 3], 'y': [0, 6, 0], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [6, 6, 6], 'type': 'bar', 'name': u'Montréal'},
             ],
-
-            'layout': go.Layout(
-                xaxis={'title': 'X-Axis'},
-                yaxis={'title': 'Y-Axis'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest')
+            'layout': {
+                'title': 'Dash testing Data Visualization'
+            }
         }
     )
 ])
 
 if __name__ == '__main__':
+    # Beanstalk expects it to be running on 8080.
     application.run(debug=True, port=8080)
